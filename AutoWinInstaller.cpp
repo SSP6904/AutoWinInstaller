546d9c81c71b6a13bfc196588c05c9c2c2ade848
Shaun Hoffer <75172901+SSP6904@users.noreply.github.com>
2025-11-22 21:58:34 -0500

    Update msbuild.yml
    Refactor and improve AutoWinInstaller functionality
    
    - Removed redundant comments and `_CRT_SECURE_NO_WARNINGS`.
    - Renamed `optionSel` to `optionSelect` and `createFi` to `createFile`.
    - Updated `optionSelect` to use `_getch()` for immediate input handling.
    - Refactored `WriteImg` for better input validation and UX.
    - Integrated `setupDisk` functionality into `optionSelect`.
    - Fixed grammar in `commandRun` error message.
    - Upgraded `PlatformToolset` to `v145` in project file.

--- AutoWinInstaller.cpp
+++ AutoWinInstaller.cpp
@@ -1,5 +1,3 @@
-// AutoWinInstaller.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <windows.h>
@@ -13,63 +11,64 @@
 
 using namespace std;
 
-void setupDisk();
 void WriteImg();
-void optionSel();
+void optionSelect();
 void commandRun(string cmd);
 void backMenu();
-void createFi(string file, string text);
+void createFile(string file, string text);
 
 int main()
 {
     SetConsoleTitle(L"Automated Windows Installer");
 	cout << "Welcome to the Automated Windows Installer!\n";
-    cout << "==============================================\n";
     cout << "Created by SSP6904 (https://shaunhoffer.cc)\n";
     cout << "If you're ready to go, press the enter key to continue! Otherwise, you may close this application!";
 	if (cin.ignore()) {
 		system("cls");
-		optionSel();
+        optionSelect();
 	}
 }
 
 void commandRun(string cmd) {
     int runCmd = system(cmd.c_str());
     if (runCmd != 0) {
-        cout << "Something went wrong while trying to run this command! Sorry about that! If this is an issue with the program, please submit it to the programs GitHub repository!";
+        cout << "Something went wrong while trying to run this command! Sorry about that! If this is an issue with the program, please submit it to this program's GitHub repository!";
         cout << "Press any key to exit the program";
         cin.ignore();
         exit(-1);
     }
 }
 
-void optionSel() {
-	int option = 0;
+void optionSelect() {
+    int option;
     cout << "1 - Setup the disk\n";
     cout << "2 - Write the image file\n";
     cout << "3 - Quit the application\n";
     cout << "Enter the number: ";
-    cin >> option;
-    switch (option) {
-        // Setup the disk
-	    case 1:
-		    system("cls");
-		    setupDisk();
-		    break;
-	    // Write the image file
-        case 2:
+    while (true) {
+        option = _getch();
+        if (option == '1') {
             system("cls");
-		    WriteImg();
-		    break;
-	    // Quit the application
-	    case 3:
-		    exit(-1);
-	    // Invalid option (default)
-        default:
+            cout << "Check the README for how you can setup your disk!\n";
+            system("diskpart");
+            cout << "\n";
+            cout << "Please be sure that you know what your drive letters that you set for your disk setup! \n";
+            cout << "If you know what they are, you may exit this step!";
+            backMenu();
+        }
+        else if (option == '2') {
+            system("cls");
+            WriteImg();
+        }
+        else if (option == '3') {
+            exit(-1);
+        }
+        else {
             cout << "Invalid option! Please try again!";
             system("pause>nul");
             system("cls");
-            optionSel();
+            optionSelect();
+        }
     }
 }
 
@@ -86,49 +85,50 @@ void WriteImg() {
 		WriteImg();
 	}
     int indexImg;
-    string gptan;
+    int gptan;
+    gptan = _getch();
 
 	// Grabs the data from the image file and asks for the index number
     commandRun("dism /Get-WimInfo /WimFile:" + imgFile + "");
-    cout << "Input your index number from above: ";
+    cout << "Input the index number from above: ";
     cin >> indexImg;
 
 	// Asks if the disk partition scheme is MBR or GPT
-    cout << "Did you use MBR or GPT for disk partition scheme? [m/g] ";
-    cin >> gptan;
+    cout << "Are you using either MBR or GPT for the disk partition scheme? [m/g] ";
     
-	// Choose this if the partion scheme is GPT
-    if (gptan == "g") {
-        string efiPar;
-        string winPar;
-        cout << "Input the EFI partiton letter: ";
-        cin >> efiPar;
-        cout << "Input the Windows partition letter: ";
-        cin >> winPar;
-        cout << "Starting the image writing! Please do not close this window while the writing is in progress!\n";
-        commandRun("dism /apply-image /imagefile:" + imgFile + " /index:" + to_string(indexImg) + " /applydir:" + winPar + ":""\"");
-        cout << "Adding the required boot files. Please wait!\n";
-        commandRun("bcdboot " + winPar + ":""\\Windows"" /s " + efiPar + ":"" /f UEFI");
-        system("cls");
-        cout << "Image write was successful! Press the enter key to go back to the main menu!";
-        backMenu();
-    }
-
-	// Choose this if the partion scheme is MBR
-    if (gptan == "m") {
-        string winPar;
-        string sysresPar;
-        cout << "Input the Windows partition letter: ";
-        cin >> winPar;
-        cout << "Input the System Reserved partition letter: ";
-        cin >> sysresPar;
-        cout << "Starting the image writing! Please do not close this window while the writing is in progress!\n";
-        commandRun("dism /apply-image /imagefile:" + imgFile + " /index:" + to_string(indexImg) + " /applydir:" + winPar + ":""\"");
-        cout << "Adding the required boot files. Please wait!\n";
-        commandRun("bcdboot " + winPar + ":""\\Windows"" /s " + sysresPar + ":"" /f BIOS");
-        system("cls");
-        cout << "Image write was successful! Press the enter key to go back to the main menu!";
-        backMenu();
+    while (true) {
+        // Choose this if the partion scheme is GPT
+        if (gptan == 'g' || gptan == 'G') {
+            string efiPar;
+            string winPar;
+            cout << "Input the EFI partiton letter: ";
+            cin >> efiPar;
+            cout << "Input the Windows partition letter: ";
+            cin >> winPar;
+            cout << "Starting the image writing! Please do not close this window while the writing is in progress!\n";
+            commandRun("dism /apply-image /imagefile:" + imgFile + " /index:" + to_string(indexImg) + " /applydir:" + winPar + ":""\"");
+            cout << "Adding the required boot files. Please wait!\n";
+            commandRun("bcdboot " + winPar + ":""\\Windows"" /s " + efiPar + ":"" /f UEFI");
+            system("cls");
+            cout << "Image write was successful! Press the enter key to go back to the main menu!";
+            backMenu();
+        }
+        // Choose this if the partion scheme is MBR
+        else if (gptan == 'm' || gptan == 'M') {
+            string winPar;
+            string sysresPar;
+            cout << "Input the Windows partition letter: ";
+            cin >> winPar;
+            cout << "Input the System Reserved partition letter: ";
+            cin >> sysresPar;
+            cout << "Starting the image writing! Please do not close this window while the writing is in progress!\n";
+            commandRun("dism /apply-image /imagefile:" + imgFile + " /index:" + to_string(indexImg) + " /applydir:" + winPar + ":""\"");
+            cout << "Adding the required boot files. Please wait!\n";
+            commandRun("bcdboot " + winPar + ":""\\Windows"" /s " + sysresPar + ":"" /f BIOS");
+            system("cls");
+            cout << "Image write was successful! Press the enter key to go back to the main menu!";
+            backMenu();
+        }
     }
 }
 
@@ -140,19 +140,10 @@ void backMenu() {
     main();
 }
 
-void createFi(string file, string text) {
+void createFile(string file, string text) {
     ofstream fileSel(file);
     fileSel << text;
     fileSel.close();
-}
-
-void setupDisk() {
-    cout << "Check the README for how you can setup your disk!\n";
-    system("diskpart");
-    cout << "\n";
-    cout << "Please be sure that you know what your drive letters that you set for your disk setup! \n";
-    cout << "If you know what they are, you may exit this step!";
-    backMenu();
 }
 
 //ofstream seldisk("seldisk.txt");
